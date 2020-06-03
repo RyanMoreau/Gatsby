e008c3091ac4ff97037fa8eba41c9c25ec6de156
devolasvegas <devondaviau@yahoo.ca>
2020-06-02 22:22:02 -0400

    import graphql and usestaticquery since Home.jsx is not a page component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,29 @@
-import React, { graphql } from "react";
+import React from "react";
+// I dunno why I didn't see this before, but you were importing graphql form react
+// Should be from gatsty
+import { graphql, useStaticQuery } from "gatsby";
 import Img from "gatsby-image";
 
-const Home = ({data}) => (
-  <div className="home">
-    <h1>
-      Home Page
-    </h1>
-    <Img fluid={data.file.childImageSharp.fluid} alt="Testing" />
-  </div>
-);
-
-export default Home;
-
-export const query = graphql `
-  query {
-    file(relativePath: {eq: "test-fox.png"}) {
-      id
-      childImageSharp {
-        fluid(maxWidth: 750) {
-          ...GatsbyImageSharpFluid
+const Home = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "test-fox.png" }) {
+        id
+        childImageSharp {
+          fluid(maxWidth: 750) {
+            ...GatsbyImageSharpFluid
+          }
         }
       }
     }
-  }
-`
+  `);
+
+  return (
+    <div className="home">
+      <h1>Home Page</h1>
+      <Img fluid={data.file.childImageSharp.fluid} alt="Testing" />
+    </div>
+  );
+};
+
+export default Home;
